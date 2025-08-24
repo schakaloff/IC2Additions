0152a4ddffcf0142bbd08306cdb5830a3f6cc590
schakaloff <filipshakalau@gmail.com>
2025-08-24 00:38:03 -0700

    new energy crystal

--- src/main/java/com/ic2additions/init/ItemInit.java
+++ src/main/java/com/ic2additions/init/ItemInit.java
@@ -1,5 +1,6 @@
 package com.ic2additions.init;
 
+import com.ic2additions.objects.items.CobaltCrystal;
 import com.ic2additions.objects.items.ItemBase;
 import net.minecraft.item.Item;
 
@@ -9,4 +10,5 @@ import java.util.List;
 public class ItemInit {
     public static final List<Item> ITEMS = new ArrayList<Item>();
     public static final Item COBALT_INGOT = new ItemBase("cobalt_ingot");
+    public static final Item COBALT_CRYSTAL = new CobaltCrystal("cobalt_crystal");
 }
